9cd60858c81765dfdb0d67de2a774b6ed320aabc
AnimeLover2119 <francoongzhangyu@gmail.com>
2023-12-07 08:17:40 +0800

    Worksheets
    Reflection

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
@@ -5,52 +5,113 @@ using System.Linq;
 
 public class SoccerPlayer : MonoBehaviour
 {
-    //public bool IsCaptain = false;
-    //public SoccerPlayer[] OtherPlayers;
-    //public float rotationSpeed = 1f;
+    public bool IsCaptain = false;
+    public SoccerPlayer[] OtherPlayers;
+    public float rotationSpeed = 1f;
 
-    //float angle = 0f;
+    float angle = 0f;
 
-    //private void Start()
-    //{
-       
-    //}
+    private void Start()
+    {
+        OtherPlayers = FindObjectsOfType<SoccerPlayer>().Where(t => t != this).ToArray();
 
-    //float Magnitude(Vector3 vector)
-    //{
-        
-    //}
+        /*if (IsCaptain)
+        {
+            FindMinimum();
+        }*/
+    }
 
-    //Vector3 Normalise(Vector3 vector)
-    //{
-        
-    //}
+    float Magnitude(Vector3 vector)
+    {
+        return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+    }
 
-    //float Dot(Vector3 vectorA, Vector3 vectorB)
-    //{
-        
-    //}
+    Vector3 Normalise(Vector3 vector)
+    {
+        float mag = Magnitude(vector);
+        vector.x /= mag;
+        vector.y /= mag;
+        vector.z /= mag;
 
-    //SoccerPlayer FindClosestPlayerDot()
-    //{
-    //    SoccerPlayer closest = null;
+        return vector;
+    }
 
-    //    return closest;
-    //}
+    float Dot(Vector3 vectorA, Vector3 vectorB)
+    {
+        return vectorA.x * vectorB.x + vectorA.y * vectorB.y + vectorA.z * vectorB.z;
+    }
 
-    //void DrawVectors()
-    //{
-    //    foreach (SoccerPlayer other in OtherPlayers)
-    //    {
-    //        // Your code here
-    //        // ...
-    //    }
-    //}
+    SoccerPlayer FindClosestPlayerDot()
+    {
+        SoccerPlayer closest = null;
+        float minAngle = 180f;
 
-    //void Update()
-    //{
+        for (int i = 0; i < OtherPlayers.Length; i++)
+        {
+            Vector3 toPlayer = OtherPlayers[i].transform.position;
+            toPlayer = Normalise(toPlayer);
 
-    //}
+            float dot = Vector3.Dot(transform.forward, toPlayer);
+            float angle = Mathf.Acos(dot);
+            angle = angle * Mathf.Rad2Deg;
+
+            if (angle < minAngle)
+            {
+                minAngle = angle;
+                closest = OtherPlayers[i];
+            }
+        }
+
+        return closest;
+    }
+
+    void DrawVectors()
+    {
+        foreach (SoccerPlayer other in OtherPlayers)
+        {
+            Debug.DrawRay(transform.position, other.transform.position - transform.position, Color.black);
+        }
+    }
+
+    void Update()
+    {
+        DebugExtension.DebugArrow(transform.position, transform.forward, Color.red);
+
+        if (IsCaptain)
+        {
+            angle += Input.GetAxis("Horizontal") * rotationSpeed;
+            transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up);
+            Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
+
+
+            DrawVectors();
+
+            SoccerPlayer targetPlayer = FindClosestPlayerDot();
+            targetPlayer.GetComponent<Renderer>().material.color = Color.green;
+
+            foreach (SoccerPlayer other in OtherPlayers.Where(t => t != targetPlayer))
+            {
+                other.GetComponent<Renderer>().material.color = Color.white;
+            }
+        }
+    }
+
+    /*void FindMinimum()
+    {
+        float minHeight = float.MaxValue;
+
+        for (int i = 0; i < 10; i++)
+        {
+            float height = Random.Range(5f, 20f);
+            Debug.Log(height);
+
+            if (height < minHeight)
+            {
+                minHeight = height;
+            }
+        }
+        Debug.Log($"Minimum height is: {minHeight}");
+    }*/
 }
 
 
